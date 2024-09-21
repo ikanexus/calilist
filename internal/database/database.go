b4f28a73a04505fdf0d903ac6c7412e4d85933be
ikanexus <git+github@ika.gg>
2024-09-21 14:36:40 +0100

    filter by last updated time on kobo_reading_state instead of book_read_link

--- internal/database/database.go
+++ internal/database/database.go
@@ -82,7 +82,7 @@ WITH ranked_books AS (
 	LEFT JOIN
 		kobo_bookmark kb ON kb.kobo_reading_state_id = krs.id
 	WHERE
-		brl.last_modified > datetime('now', '-30 day')
+		krs.last_modified > datetime('now', '-30 day')
 		AND kb.progress_percent IS NOT NULL
 		AND brl.read_status != 0
 ) SELECT
